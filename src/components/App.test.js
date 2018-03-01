eae33653ee3043838b58371457c6d39b96f7b7ad
Kevin Egstorf <kevin.egstorf@gmail.com>
2018-03-01 21:11:08 +0100

    first commit

---
+++ src/components/App.test.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import App from './App';
+
+describe('App', () => {
+  const app = shallow(<App />);
+
+  it('renders properly', () => {
+    expect(app).toMatchSnapshot();
+  });
+
+  it('contains a connected wallet component', () => {
+// console.log(app.debug());
+    expect(app.find('Connect(Wallet)').exists()).toBe(true);
+  });
+
+  it('contains a connected Loot component', () => {
+    expect(app.find('Connect(Loot)').exists()).toBe(true);
+  });
+});
